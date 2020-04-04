5bce86bde2d90f3bba90fcbd03f7120ebd32383f
Noah <gekkoe808@gmail.com>
2020-04-03 22:04:15 -0700

    Added tools.cli for command line parsing.

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/spec.alpha "0.2.187"]
                  [org.clojure/test.check "1.0.0"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [ring/ring-core "1.8.0"]
                  [ring/ring-defaults "0.3.2"]]
 
