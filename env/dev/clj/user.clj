683e051b783b94c481230376080363b6e84197b1
Noah <gekkoe808@gmail.com>
2020-04-24 03:09:20 -0700

    Added some dependencies and helper vars to dev user ns.

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -2,14 +2,18 @@
   "Userspace functions you can run by default in your local REPL."
   (:require
    [sorted.config :refer [env]]
-    [clojure.pprint]
-    [clojure.spec.alpha :as s]
-    [clojure.java.classpath :as cp]
-    [expound.alpha :as expound]
-    #_[mount.core :as mount]
-    [sorted.core :refer :all]
-    [sorted.person :as p]
-    [sorted.fileio :as f]))
+   [clj-time.format :as ctf]
+   [clojure.pprint]
+   [clojure.spec.alpha :as s]
+   [clojure.spec.gen.alpha :as g]
+   [clojure.spec.test.alpha :as st]
+   [clojure.java.classpath :refer [classpath]]
+   [expound.alpha :as expound]
+   [failjure.core :as f]
+   #_[mount.core :as mount]
+   [sorted.core :refer :all]
+   [sorted.person :as p]
+   [sorted.fileio :as file]))
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
 
@@ -19,7 +23,13 @@
 (def john "Doe John Male Blue 1/1/1943")
 (def jane "Doe, Jane, Female, Green,     12/1/2001")
 (def june "Doe| June| Female| Red   | 2/1/1983")
-(def jim "Doe| Jim|Male| Purple   |11/21/2014")
+(def jim "Doe| Jim|Male||         11/21/2014")
+
+;; Some malformed person string
+(def bad1 "Doe| Jack|Male| Orange |111/21/2014")
+(def bad2 "Doe| Jenny|Female| Pink |")
+(def bad3 "_,|| |,|")
+(def bad4 "")
 
 #_(defn start
   "Starts application.
