02cff22b7772d02565c37b0424558d53f921ed0e
Noah <gekkoe808@gmail.com>
2020-04-24 03:11:50 -0700

    Refactored person for simplicity and readability.

--- src/sorted/person.clj
+++ src/sorted/person.clj
@@ -17,6 +17,23 @@
   [s delim]
   (map trim (split s delim)))
 
+(defn split-on-delims
+  "Attempts to split a string into a vector of strings. Attempts to use pipe,
+  comma, or whitespace as delimiters, in that order."
+  [s]
+  (cond (.contains s "|") (split-trim s #"\|")
+        (.contains s ",") (split-trim s #",")
+        :else (split-trim s #"\s+")))
+
+(defn strs->vals
+  "Attempts to convert a seq of 5 strings to vals that can be converted into a
+  person.
+  Returns a Failure object if unsuccessful."
+  [ss]
+  (let [formatter (ctf/formatter "MM/dd/yyyy")]
+    (f/try* (conj (vec (drop-last ss))
+                  (ctf/parse formatter (last ss))))))
+
 (defn vals->person
   "Given a vector of values, creats a map merging those values with the person
   keywords."
@@ -30,34 +47,36 @@
   Returns a map that conforms to the :sorted.person/person spec or a Failure
   object."
   [s]
-  (let [raw-vals (cond (.contains s "|") (split-trim s #"|")
-                       (.contains s ",") (split-trim s #",")
-                       :else (split-trim s #"\s+"))
-        ;; TODO: Put failjure try clause in here for exceptions thrown by
-        ;;       clj-time on bad dates.
-        vals (conj (vec (drop-last raw-vals)) (ctf/parse (last raw-vals)))
-        person (vals->person vals)]
-    (if (s/valid? ::person person)
-      person
-      (f/fail "Error in str->person: Could not parse \"%s\"" s))))
+  (f/if-let-ok? [person (f/ok-> s
+                                split-on-delims
+                                strs->vals
+                                vals->person)]
+                person
+                (f/fail "Error in str->person: Could not parse `%s`\n%s"
+                        s
+                        (.getMessage person))))
 
 ;;;============================================================================
 ;;;                              S P E C S
 ;;;============================================================================
-(s/def ::raw (s/and string? (complement no-delims?))) ; Unparsed str of a person
+
+;; The following spec deliberately allows invalid strings for testing purposes.
+;; Validation will occur further through the process of conversion to a person.
+(s/def ::maybeperson (s/and string?))
 
 ;;; TODO: Add some generators
-(s/def ::last-name (s/and string? no-delims?))
-(s/def ::first-name (s/and string? no-delims?))
-(s/def ::gender (s/and string? no-delims?))
-(s/def ::fav-color (s/and string? no-delims?))
+(s/def ::no-delim-str (s/and string? no-delims?))
+(s/def ::last-name ::no-delim-str)
+(s/def ::first-name ::no-delim-str)
+(s/def ::gender ::no-delim-str)
+(s/def ::fav-color ::no-delim-str)
 (s/def ::dob inst?)
 
 (s/def ::person (s/keys
                  :req [::first-name ::last-name ::gender ::fav-color ::dob]))
 
 (s/fdef no-delims?
-  :args (s/cat :s ::raw)
+  :args (s/cat :s ::maybeperson)
   :ret boolean?
   :fn (s/or :true (s/and #(= (:ret %) true)
                          #(not (.contains (-> % :args :s) " "))
