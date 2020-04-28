0a91aabf3d9e310ddd05f7508ccc99234d81e9f3
Noah <gekkoe808@gmail.com>
2020-04-28 07:19:34 -0700

    Updated person tests to use pre-defined formatter.

--- test/sorted/person_test.clj
+++ test/sorted/person_test.clj
@@ -5,12 +5,12 @@
             [clojure.spec.gen.alpha :as g]
             [java-time :as jt]))
 
-(deftest person-exercise-test
+(deftest person-test
   (let [valid-person {::p/last-name "Doe"
                       ::p/first-name "John"
                       ::p/gender "Male"
                       ::p/fav-color "Blue"
-                      ::p/dob (jt/local-date "MM/dd/yyyy" "01/22/1972")}
+                      ::p/dob (jt/local-date p/formatter "01/22/1972")}
         invalid-person {:x 3 :y 9}]
     (testing "Checking if a person conforms to spec"
       (is (s/valid? ::p/person valid-person)))
