cbb4c6436241857f810dd469e8f63fccfe54590f
Noah <gekkoe808@gmail.com>
2020-05-01 09:27:01 -0700

    Refactored filio for consistency with the restg of the code.

--- src/sorted/fileio.clj
+++ src/sorted/fileio.clj
@@ -8,18 +8,17 @@
   "Tests if a file exists.
   Returns an Failure object if an exception is thrown."
   [file-name]
-  (try
-    (.exists (io/file file-name))
-    (catch Exception e
-      (f/fail "Error in exists?: %s" (.getMessage e)))))
+  (f/if-let-ok? [exists? (f/try* (.exists (io/file file-name)))]
+                exists?
+                (f/fail "Error in exists?: %s" (f/message exists?))))
 
 (defn text-read
   "Returns a vector containing the lines of file-name if it exists.
   The output, if file-name is a binary file, is undefined.
   Returns a Failure object if an exception is thrown."
   [file-name]
-  (try
-    (with-open [rdr (BufferedReader. (FileReader. file-name))]
-      (reduce conj [] (line-seq rdr)))
-    (catch Exception e
-      (f/fail "Error in text-read: %s" (.getMessage e)))))
+  (f/if-let-ok? [text (f/try* (with-open
+                                [rdr (BufferedReader. (FileReader. file-name))]
+                                (vec (line-seq rdr))))]
+                text
+                (f/fail "Error in text-read: %s" (f/message text))))
