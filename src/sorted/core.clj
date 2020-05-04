cafb45dae5a8b9ffe2adc41d96a32d2a007a2dcf
Noah <gekkoe808@gmail.com>
2020-05-03 18:13:36 -0700

    Preliminary code for parsing/sorting/displaying in core.

--- src/sorted/core.clj
+++ src/sorted/core.clj
@@ -6,10 +6,7 @@
             [failjure.core :as f])
   (:gen-class))
 
-(def usage
-  (str "Usage: sorted [options] file1 file2 file3\n"
-       "This program can support more than 3 files if desired, "
-       "but at least one must be provided.\n\n"))
+(def people (atom []))
 
 (def cli-options
   ;; An option with a required argument
@@ -21,21 +18,84 @@
    ["-l" "--last" "Sort output by last name, descending."]
    ["-h" "--help" "Prints this help text.\n\n"]])
 
+(defn usage [options-summary]
+  (->> ["sorted - A simple program for sorting people."
+        ""
+        "Usage: sorted [options] file1 file2 file3 ..."
+        ""
+        "Options:"
+        options-summary
+        ""
+        "This program can support more than 3 files if desired, "
+        "but at least one must be provided.\n\n"]
+       (join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (join \newline errors)))
+
+(defn validate-args
+  "Validate command line arguments. Either return a map indicating the program
+  should exit (with a error message, and optional ok status), or a map
+  indicating the order to sort output and the files for input."
+  [args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
+        {:keys [help dob gender last]} options
+        sort-count (->> [dob gender last]
+                        (filter true?)
+                        count)]
+    (cond
+      (:help options) ; help => exit OK with usage summary
+      {:exit-message (usage summary) :ok? true}
+      errors ; errors => exit with description of errors
+      {:exit-message (error-msg errors)}
+      ;; Make sure no more than one sort option in indicated
+      (> sort-count 1)
+      {:exit-message "Please select no more than one sort option."}
+      (empty? arguments)
+      {:exit-message "Please provide at least one file with people records in it."}
+      :else {:sort-kw (cond dob    ::p/dob
+                            gender ::p/gender
+                            last   ::p/last-name
+                            :else  nil)
+             :files arguments})))
+
+(defn exit [status msg]
+  (println msg)
+  #_(System/exit status))
+
+(defn load-files!
+  [files]
+  (swap! people (fn [_] (vec
+                         (->> (map file/text-read files)
+                              (map (partial map p/str->person))
+                              flatten
+                              (remove f/failed?))))))
+
+(defn sort-people
+  [sort-kw]
+  (if-let [order (case sort-kw
+                   ::p/dob compare
+                   ::p/gender compare
+                   ::p/last-name #(compare %2 %1)
+                   nil)]
+    (vec (sort-by sort-kw order @people))
+    @people))
+
 (defn -main
   "Expects to be passed in files in one of three formats found in the fileio
   namespace. Parses these files and prints out the people in them sorted one of
   three ways depending on which command line option is selected."
   [& args]
-  (let [opts (parse-opts args cli-options)
-        file-names (:arguments opts)]
-    (cond
-      (or (get-in opts [:options :help])
-          (empty? file-names)) (println (str usage (:summary opts)))
-      (:errors opts) (println (join "\n" (:errors opts)))
-      :else (->> (map file/text-read file-names)
-                 (map (partial map p/str->person))
-                 flatten
-                 (remove f/failed?)))))
+  (let [{:keys [sort-kw files exit-message ok?]} (validate-args args)]
+    (if exit-message
+      (exit (if ok? 0 1) exit-message)
+      (when (load-files! files)
+        (->> (sort-people sort-kw)
+             (map #(p/person->str % " "))
+             (join \newline)
+             print)))))
+
 
 (comment
   (map #(p/person->str % " ") (sort-by ::p/dob #(compare %2 %1)
