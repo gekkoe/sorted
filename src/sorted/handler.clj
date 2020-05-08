1e712e512b57c5344a47affb1e37d18f002062e5
Noah <gekkoe808@gmail.com>
2020-05-07 21:16:26 -0700

    More functionality in handler ns.

--- src/sorted/handler.clj
+++ src/sorted/handler.clj
@@ -1,11 +1,15 @@
 (ns sorted.handler
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
-            [cheshire.core :refer :all]
             [clojure.pprint :refer [pprint]]
-            [compojure.core :refer [defroutes GET POST]]
+            [clojure.string :refer [join]]
+            [compojure.core :refer [defroutes ANY GET POST]]
+            [compojure.route :refer [not-found]]
             [sorted.person :as p]
-            [sorted.people :refer [sorted-by]])
+            [sorted.people :refer [sorted-by]]
+            [sorted.people :as ppl]
+            [clojure.spec.alpha :as s]
+            [failjure.core :as f])
   (:import java.io.StringWriter))
 
 (defn pprint->string [x]
@@ -13,30 +17,65 @@
     (pprint x w)
     (.toString w)))
 
+(def site-map-msg
+  (->> ["This site supports the following operations:"
+        "POST <a href=\"/records\">/records</a> - Add a person to the site."
+        (str "GET <a href=\"/records/gender\">/records/gender</a>"
+             " - Sort by gender, female first.")
+        (str "GET <a href=\"/records/birthdate\">/records/birthdate</a>"
+             " - Sort by birth date ascending, then by last name ascending.")
+        (str "GET <a href=\"/records/name\">/records/name</a>"
+             " - Sort by last name descending.")]
+       (join "<br>")))
+
 (defn sorted-resource
   [kw]
   (resource
    :available-media-types ["application/json"]
    :exists? (fn [ctx]
               (try
-                {::data (generate-string
-                         {:people (map p/person->un-map
-                                       (sorted-by kw))})}
+                {::data {:people (map p/person->un-map (sorted-by kw))}}
                 (catch Exception e
                   [false {::error (.getMessage ^Throwable e)}])))
    :handle-ok ::data
-   :handle-not-found ::error))
+   :handle-not-found ::error
+   :etag (fn [_] (str (count @ppl/people)))))
+
+(defn parse-person-str [ctx key]
+  (when (= :post (-> ctx :request :request-method))
+    (if-let [body (slurp (-> ctx :request :body))]
+      (if (s/valid? ::p/person-str body)
+        (f/if-let-ok?
+         [person (p/str->person body)]
+         [false {key person}]
+         {:message (str "Person string malformed. Should be in the form "
+                        "Last|First|Gender|FavColor|BirthDate "
+                        "where | can be replaced by space or comma and "
+                        "birth date is in M/d/yyyy format.")})))))
 
 (defroutes app
+  (GET "/" request site-map-msg)
+  (ANY "/records" []
+       (resource
+        :allowed-methods [:post :get]
+        :available-media-types ["text/html" "application/json"]
+        :handle-ok "<html>Post text/plain to this resource.<br>\n"
+        :malformed? #(parse-person-str % ::data)
+        :post! (fn [ctx] (swap! ppl/people conj (::data ctx)))))
   (GET "/records/gender" request
        (sorted-resource ::p/gender))
   (GET "/records/birthdate" request
        (sorted-resource ::p/dob))
   (GET "/records/name" request
        (sorted-resource ::p/last-name))
-  (GET "/test" request
-       (pprint->string request)))
+  (not-found (str "Page not found.<br>" site-map-msg) ))
 
 (def handler
   (-> app
       wrap-params))
+
+(comment
+  (require '[ring.mock.request :as rm])
+  (require '[sorted.handler :as handler])
+  (handler/handler (rm/body (rm/request :post "/records") "Aaronson,Jill,Female,Violet,10/10/1979"))
+  )
