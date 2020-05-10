f09f6e30e460c0416c0213579a38b6cf3442a6c7
Noah <gekkoe808@gmail.com>
2020-05-09 22:51:58 -0700

    Refactored people ns. Added to spec.

--- src/sorted/people.clj
+++ src/sorted/people.clj
@@ -1,21 +1,24 @@
 (ns sorted.people
   (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
             [failjure.core :as f]
             [sorted.fileio :as file]
+            [sorted.helpers :as h]
             [sorted.person :as p]))
 
 (def people (atom []))
 (def post-limit 10000) ; Used to limit online posts to people
 
 (defn load-from-files!
-  "Loads files and attempts to parse them as :sorted.person/person values and save
-  the collection of them in sorted.people/people. Ignores any lines that it
-  cannot parse."
+  "Expects a collection of strings containing file names. Loads files and attempts
+  to parse them as :sorted.person/person values and save the collection of them
+  in sorted.people/people. Ignores any lines that it cannot parse."
   [files]
-  (swap! people (fn [_] (vec (->> (map file/text-read files)
-                                  (map (partial map p/str->person))
-                                  flatten
-                                  (remove f/failed?))))))
+  (reset! people (->> (map file/text-read files)
+                      flatten
+                      (map p/str->person)
+                      (remove f/failed?)
+                      vec)))
 
 (defn sorted-by
   [sort-kw]
@@ -30,11 +33,10 @@
 
 (defn people->strs [ppl] (map #(p/person->str % " ") ppl))
 
+(def sort-kws #{::p/gender ::p/dob ::p/last-name})
 (s/def ::sort-kw (s/with-gen (s/nilable keyword?)
-                   (constantly (s/gen #{::p/gender
-                                        ::p/dob
-                                        ::p/last-name
-                                        nil}))))
+                   #(gen/one-of [(s/gen sort-kws)
+                                 (gen/return nil)])))
 
 (s/fdef sorted-by
   :args (s/cat :sort-kw ::sort-kw)
@@ -43,4 +45,7 @@
 (s/fdef load-from-files!
   :args (s/cat :files (s/coll-of string? :into []))
   :ret (s/or :success (s/coll-of ::p/person :into [])
-             :failure f/failed?))
+             :failure f/failed?)
+  :fn #(if (= (-> % :ret first) :success)
+         (= (-> % :ret second) @people)
+         true))
