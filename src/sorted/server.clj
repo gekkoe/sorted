e093534de737d3f26a571e3b57b8b1ccb1678039
Noah <gekkoe808@gmail.com>
2020-05-11 21:39:08 -0700

    Added error checking and tests to server ns.

--- src/sorted/server.clj
+++ src/sorted/server.clj
@@ -1,24 +1,53 @@
 (ns sorted.server
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [sorted.handler :refer [handler]]))
+  (:require [failjure.core :as f]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [sorted.handler :refer [handler]]
+            [clojure.spec.alpha :as s]))
 
 (def server (atom nil))
 (def default-port 3000)
 
+(defn get-port
+  "Returns the port of the running server. Returns nil if no server is running."
+  []
+  (when @server
+    (let [cs (. ^org.eclipse.jetty.server.Server @server getConnectors)
+          c (first cs)
+          port (. ^org.eclipse.jetty.server.ServerConnector c getLocalPort)]
+      port)))
+
 (defn start-server!
+  "Attempts to start the web server on port. If no port is passed, defaults to
+  sorted.server/default-port. Returns an org.eclipse.jetty.server.Server object
+  on success.
+  Returns a Failure object if unsuccessful."
   ([] (start-server! default-port))
   ([port]
-   (if @server
-     (.start ^org.eclipse.jetty.server.Server @server)
-     (let [svr (run-jetty handler {:port port :join? false})]
-       (reset! server svr)))))
+   (f/if-let-ok? [result
+                    (f/try*
+                     (if @server
+                       @server
+                       (let [svr (run-jetty handler {:port port :join? false})]
+                         (reset! server svr))))]
+                   result
+                   (f/fail (format "Unable to open server on port %s\n%s"
+                                   port
+                                   (f/message result))))))
 
-(defn stop-server! [] (if @server
-                        (do
-                          (.stop ^org.eclipse.jetty.server.Server @server)
-                          (reset! server nil))))
+(defn stop-server!
+  "If the web server is running, will stop it.
+  Returns nil on success.
+  Returns a Failure object if something goes wrong."
+  []
+  (f/try*
+   (when @server
+     (do
+       (.stop ^org.eclipse.jetty.server.Server @server)
+       (reset! server nil)))))
 
 (defn restart-server!
+  "Stops & restarts the web server if one is running. Otherwise starts a new one.
+  Returns a Failure object if something goes wrong."
   ([] (restart-server! default-port))
   ([port]
    (stop-server!)
---
+++ test/sorted/server_test.clj
@@ -0,0 +1,43 @@
+(ns sorted.server-test
+  (:require [clojure.test :refer :all]
+            [failjure.core :as f]
+            [sorted.helpers :as h]
+            [sorted.server :as svr]))
+
+;; NOTE: The double with-defs clauses here are to prevent the reset! operation in
+;;       start-server! from seeing the wrong server, which causes tests to step
+;;       on the real active server.
+
+(deftest get-port-test
+  (let [port (h/get-free-port)]
+    (with-redefs [svr/server (atom nil)]
+      (with-redefs [svr/server (atom (svr/start-server! port))]
+        (testing "Returns the correct port"
+          (is (= port (svr/get-port))))
+
+        ;; Be sure to stop test server before leaving with-redefs
+        (svr/stop-server!))))
+
+  (deftest start-server!-test
+    (let [port (h/get-free-port)]
+      (with-redefs [svr/server (atom nil)]
+        (testing (str "Returns an org.eclipse.jetty.server.Server object on "
+                      "success. Returns a Failure object if something fails.")
+          (let [server (svr/start-server! port)]
+            (is (instance? org.eclipse.jetty.server.Server server))
+            (svr/stop-server!)))
+        (testing "Returns a Failure object on failure"
+          (let [impossible-port 90000]
+            (is (f/failed? (svr/start-server! impossible-port)))))
+
+        ;; Be sure to stop test server before leaving with-redefs
+        (svr/stop-server!)))))
+
+(deftest stop-server!-test
+  (with-redefs [svr/server (atom nil)]
+    (with-redefs [svr/server (atom (svr/start-server! (h/get-free-port)))]
+      (let [result (svr/stop-server!)]
+        (testing "Sets svr/server to nil"
+          (is (nil? @svr/server)))
+        (testing "Returns nil"
+          (is (nil? result)))))))
